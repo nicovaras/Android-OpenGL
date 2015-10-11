357411670efd80e3d7de42e2625a3002a104438f
Nico Varas <nicovaras22@gmail.com>
2015-10-11 20:32:40 -0300

    Create basic shaders
    Basic vertex and fragment shaders, no functionality yet.

--- app/src/main/java/com/example/wert/opengl/OpenGLRenderer.java
+++ app/src/main/java/com/example/wert/opengl/OpenGLRenderer.java
@@ -1,14 +1,52 @@
 package com.example.wert.opengl;
 
 import android.opengl.GLSurfaceView;
-import static android.opengl.GLES20.*;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+
 public class OpenGLRenderer implements GLSurfaceView.Renderer {
+    private static final int POSITION_COMPONENTS = 2;
+    private static final int BYTES_FLOAT= 4;
+    private final FloatBuffer vertexData;
+
+    public OpenGLRenderer(){
+        float[] tableVertices = {
+                // Board
+                0f, 0f,
+                9f, 14f,
+                0f, 14f,
+
+                0f, 0f,
+                9f, 0f,
+                9f, 14f,
+
+                // Line
+                0f, 7f,
+                9f, 7f,
+
+                //Players
+                4.5f, 2f,
+                4.5f, 12f
+        };
+        vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_FLOAT)
+                               .order(ByteOrder.nativeOrder())
+                               .asFloatBuffer();
+        vertexData.put(tableVertices);
+    }
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f );
+        glClearColor(0.098f, 0.098f, 0.439f, 0.0f );
     }
 
     @Override
