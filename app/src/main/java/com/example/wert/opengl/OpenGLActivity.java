93e014874d49f763a110bb5ded2507fe018c6195
Nico Varas <nicovaras22@gmail.com>
2015-10-10 21:02:11 -0300

    Draw a red screen
    Basic renderer and OponGL activity that draws a red screen each frame.

---
+++ app/src/main/java/com/example/wert/opengl/OpenGLActivity.java
@@ -0,0 +1,69 @@
+package com.example.wert.opengl;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class OpenGLActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glSurfaceView = new GLSurfaceView(this);
+        final ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo =
+                activityManager.getDeviceConfigurationInfo();
+        final boolean supportsOGL = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if(supportsOGL){
+            glSurfaceView.setEGLContextClientVersion(2);
+            glSurfaceView.setRenderer(new OpenGLRenderer());
+            rendererSet = true;
+        }
+
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(rendererSet){
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(rendererSet){
+            glSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_open_gl, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/wert/opengl/OpenGLRenderer.java
@@ -0,0 +1,23 @@
+package com.example.wert.opengl;
+
+import android.opengl.GLSurfaceView;
+import static android.opengl.GLES20.*;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class OpenGLRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f );
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
